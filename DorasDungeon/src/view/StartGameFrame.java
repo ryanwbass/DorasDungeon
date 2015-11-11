75271b9f18bc538d39c2e274fb78859987703bbc
Kaitlyn Dodds <kzd5123@psu.edu>
2015-11-11 00:51:42 -0800

    Fixed small bug in StartGameFrame

--- DorasDungeon/src/view/StartGameFrame.java
+++ DorasDungeon/src/view/StartGameFrame.java
@@ -1,32 +1,51 @@
 package view;
 
+import controller.OptionsController;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import model.OptionsModel;
 
 /**
  * Created by Katie on 11/2/2015.
  */
-public class StartGameFrame extends JFrame {
+public class StartGameFrame extends JPanel implements ActionListener {
 
     private JPanel panel;
+    private OptionButton returnButton;
 
-    public StartGameFrame() {
-
-        panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-        panel.setBackground(Color.DARK_GRAY);
+    private MyJPanel parentPanel;
+    
+    public StartGameFrame(MyJPanel parentPanel) {
+        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        this.setBackground(Color.DARK_GRAY);
+        
+        this.parentPanel = parentPanel;
+        
+        returnButton = new OptionButton();
+        returnButton.setText("Options Menu");
+        returnButton.addActionListener(this);
+        returnButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        this.add(returnButton);
+        this.add(Box.createVerticalStrut(20));
 
         JLabel label = new JLabel("Game Should Start Now");
         label.setFont(new Font("Ariel", Font.PLAIN, 20));
         label.setForeground(Color.CYAN);
-        panel.add(label);
-
-        this.setContentPane(panel);
-        this.setLocationRelativeTo(null);
-        this.setSize(400, 100);
-        this.setTitle("Start Game Option");
-        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+        this.add(label);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        parentPanel.clearTopPanel(this);
+                    
+        parentPanel.add(parentPanel.optionsView, BorderLayout.CENTER);
+        
+    }
+    
+    
 
 }
