15672a02cb93538b893fc349f45a534492bbb40e
dil5315 <dil5315@vlabs-web02.up.ist.local>
2015-12-16 15:43:42 -0500

    Everything Works but its not linked with the difficulty. Need Help

--- DorasDungeon/src/view/InstructionsView.java
+++ DorasDungeon/src/view/InstructionsView.java
@@ -17,6 +17,9 @@ import javax.swing.*;
 public class InstructionsView extends JPanel{
     
     public OptionButton returnButton;
+    JLabel line1;
+    JLabel line2;
+    JLabel line3;
     
      
     public InstructionsView() {
@@ -24,12 +27,26 @@ public class InstructionsView extends JPanel{
 
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         this.setBackground(Color.DARK_GRAY);
-
-        // set up Btns on View
+        
+        line1 = new JLabel("Using the arrow keys, guide Dora to the checkered flag space to win the game.");
+        line1.setForeground(Color.CYAN);
+        
+        line2 = new JLabel("But watch out! If you fail to make it to the end before the turn count hits 0, you lose the game.");
+        line2.setForeground(Color.CYAN);
+        
+        line3 = new JLabel("Collect the Yellow Orbs to increase your score as you move through the dungeon.");
+        line3.setForeground(Color.CYAN);
+        
         returnButton = new OptionButton();
         returnButton.setText("Return to Main Menu");
-        this.add(returnButton);
+        
+        this.add(line1);
         this.add(Box.createVerticalStrut(20));
+        this.add(line2);
+        this.add(Box.createVerticalStrut(20));
+        this.add(line3);
+        this.add(Box.createVerticalStrut(20));
+        this.add(returnButton);
         
         
         
